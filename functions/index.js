da3065c8c11ad21e64ff188966e2f5d9d8292f06
robpwatkins <robpwatkins@gmail.com>
2021-03-31 21:25:35 -0500

    its hosted with a custom domainnnnn

--- functions/index.js
+++ functions/index.js
@@ -10,89 +10,93 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-app.get('/ping', (req, res) => {
-  return res.status(200).send('pong!');
-});
+exports.tester = functions.https.onRequest((req, res) => {
+  res.status(200).send('heyoo');
+})
 
-app.get('/api/read/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      let item = await document.get();
-      let response = item.data();
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+// app.get('/ping', (req, res) => {
+//   return res.status(200).send('pong!');
+// });
 
-app.get('/api/read', (req, res) => {
-  (async () => {
-    try {
-      let query = db.collection('items');
-      let response = [];
-      await query.get().then(querySnapshot => {
-        let docs = querySnapshot.docs;
-        for (let doc of docs) {
-          const selectedItem = {
-            id: doc.id,
-            item: doc.data().item
-          };
-          response.push(selectedItem);
-        }
-      });
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+// app.get('/api/read/:item_id', (req, res) => {
+//   (async () => {
+//     try {
+//       const document = db.collection('items').doc(req.params.item_id);
+//       let item = await document.get();
+//       let response = item.data();
+//       return res.status(200).send(response);
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
 
-app.put('/api/update/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      await document.update({
-        item: req.body.item
-      });
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+// app.get('/api/read', (req, res) => {
+//   (async () => {
+//     try {
+//       let query = db.collection('items');
+//       let response = [];
+//       await query.get().then(querySnapshot => {
+//         let docs = querySnapshot.docs;
+//         for (let doc of docs) {
+//           const selectedItem = {
+//             id: doc.id,
+//             item: doc.data().item
+//           };
+//           response.push(selectedItem);
+//         }
+//       });
+//       return res.status(200).send(response);
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
 
-app.delete('/api/delete/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      await document.delete();
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+// app.put('/api/update/:item_id', (req, res) => {
+//   (async () => {
+//     try {
+//       const document = db.collection('items').doc(req.params.item_id);
+//       await document.update({
+//         item: req.body.item
+//       });
+//       return res.status(200).send();
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
 
-app.post('/api/create', (req, res) => {
-  (async () => {
-    try {
-      await db.collection('items').doc('/' + req.body.id + '/').create({item: req.body.item});
-      console.log('here:', db.collection('items').doc());
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+// app.delete('/api/delete/:item_id', (req, res) => {
+//   (async () => {
+//     try {
+//       const document = db.collection('items').doc(req.params.item_id);
+//       await document.delete();
+//       return res.status(200).send();
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
 
-exports.app = functions.https.onRequest(app);
+// app.post('/api/create', (req, res) => {
+//   (async () => {
+//     try {
+//       await db.collection('items').doc('/' + req.body.id + '/').create({item: req.body.item});
+//       console.log('here:', db.collection('items').doc());
+//       return res.status(200).send();
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
+
+// exports.app = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
